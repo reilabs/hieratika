88e5d71d4d9a23aeb68ceb1e295cb6a5ea6fa5e0
Ara Adkins <iamrecursion@users.noreply.github.com>
2025-05-09 18:07:36 +0200

    Add a Cairo-compatible mangling scheme (#193)
    The previous mangling scheme was intended to operate purely in FLO and
    Sierra which do not have any care for the way things are named. In the
    interests of boxing the work to getting something running, we are
    instead hoping to target source Cairo, which requires our naming scheme
    to be compatible with Cairo names. This new mangling scheme accounts for
    that case.
    
    For the compiler's existing uses of mangling, it now mangles names using
    the cairo-compatible scheme.

--- crates/mangler/src/constants.rs
+++ crates/mangler/src/constants.rs
@@ -1,8 +1,20 @@
 //! This module contains constants that specify portions of the mangling
 //! behavior.
 
+use std::sync::LazyLock;
+
+use regex::Regex;
+
 /// A marker used to separate sections in the mangled string.
 pub const SECTION_SEPARATOR: &str = "$";
+
+/// A marker used to separate sections in the mangled string when compatibility
+/// with source-level Cairo is required.
+pub const SECTION_SEPARATOR_CAIRO: &str = "___";
+
+/// A marker used to replace a character in any user-provided string for
+/// mangling that is disallowed in a Cairo identifier.
+pub const INVALID_CAIRO_CHARACTER: &str = "_IC_";
 
 /// A marker used to signify the beginning of a snapshot mangle segment.
 pub const BEGIN_SNAPSHOT: &str = "m";
@@ -24,3 +36,8 @@ pub const END_ENUM: &str = "g";
 
 /// The prefix for symbols that indicates that they are internal or reserved.
 pub const INTERNAL_NAME_PREFIX: &str = "__";
+
+/// The regex that matches on characters that are invalid to have in a Cairo
+/// identifier.
+pub static INVALID_CAIRO_CHARS_REGEX: LazyLock<Regex> =
+    LazyLock::new(|| Regex::new("[^[A-Za-z0-9_-]]").expect("Hardcoded regex was not valid"));
